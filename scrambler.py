593660646dcecbbdbca961a2e82a532acd967126
HalynLee <rinchive@gmail.com>
2021-03-24 21:29:41 +0900

    modified find_sentence_place()

--- scrambler.py
+++ scrambler.py
@@ -3,20 +3,22 @@ import argparse
 from random import randint
 
 
-def find_sentences_place(sentences):
+def find_sentence_place(sentences):
     numofsent = len(sentences)
     sent_list = sentences
     j = 1
     choices = []
-
+    AtoD = {1:'(A)', 2:'(B)',3:'(C)', 4:'(D)'} 
+    
     #Choose a sentence
     target = randint(1 ,numofsent-1)
     target_sent = sent_list.pop(target)
 
     #Make choices number
     for i in range(-2,2):
-        num = (target + i)%4
+        num = (target + i)%(numofsent-1)
         choices.append(num)
+    choices.sort()
     print(choices)
 
     for i in range(numofsent-1) :
@@ -30,13 +32,15 @@ def find_sentences_place(sentences):
     print('In which of the positions marked [1],[2],[3] and [4] \ndoes the following sentence best belong?')
     print('\n\n"', target, target_sent , '"\n')
     print('(A) [1]\n(B) [2]\n(C) [3]\n(D) [4]\n')
-    print('\n\nAnswer =>', target)
+    print('\n\nAnswer =>', AtoD[choices.index(target-1)+1])
+
+
 
 def sentence_scramble(sentences):
     numofsent = len(sentences)
     first_sent = sentences[0]
     print("First = ", first_sent)
-    tmp = ''
+    
 
 
 def main() :
@@ -58,7 +62,8 @@ def main() :
     for i in range(len(sent_list)) :
         print(i, "th ====> ",sent_list[i])
 
-    find_sentences_place(sent_list)
+    find_sentence_place(sent_list)
+    #sentence_scramble(sent_list)
 
 if __name__ == "__main__":
     main()
